a92b89dcfe113f645dc937ca95b0efd0d7688a22
AKSHAY-0509 <gundaakshay3@gmail.com>
2026-09-17 09:08:59 +0530

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let express=require("express");
 let app=express();
-let hrRoutes=require("./routes/hr_route");
-app.use("/api/hr",hrRoutes);
 
 
+let hrroutes=require("./routes/hr_route");
+let emprouter=require("./routes/emp_route")
 
+app.use("/api/hr",hrroutes);
+app.use("/api/emp",emprouter);
 
 app.listen(3000,()=>{
-    console.log("server listening on port 3000")
+    console.log("server listening to port number 3000");
 })
